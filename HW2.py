0d3a5bd793a731e8eae072db98a052c2e8fb1c33
Cameron Beck <cameron@pixelthin.com>
2024-02-20 20:11:09 -0600

    almost fixed everthing

--- HW2.py
+++ HW2.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 import statistics
 allVals =[]
+#read in values
 with open('BostonNumbers.txt',encoding='utf-8') as f:
     idx =0 
     for line in f:
@@ -20,23 +21,30 @@ with open('BostonNumbers.txt',encoding='utf-8') as f:
         else:
             allVals[idx].extend(lineVals)
             idx+=1
-
-#calculate mean and stdev for each set 
+#split sets
 validationSet = allVals[(len(allVals)-50):]
 trainingSet = allVals[:(len(allVals)-50)]
 validationSet = np.array(validationSet)
 trainingSet = np.array(trainingSet)
-
+allVals = np.array(allVals)
 #normalize data
 for i in range(0,12):
     mean = statistics.mean(trainingSet[:,i])
+    meanVal = statistics.mean(validationSet[:,i])
     standardDeviation = statistics.stdev(trainingSet[:,i])
+    standardDeviationVal = statistics.stdev(validationSet[:,i])
     for index, number in enumerate(trainingSet[:,i]):
-        trainingSet[index,i] = abs(number-mean)/standardDeviation
+        #if number is 0, we dont need to divide, as it could give us NAN
+        if(trainingSet[index,i]!=0):
+            trainingSet[index,i] = abs(number-mean)/standardDeviation
+    for index2,number2 in enumerate(validationSet[:,i]):
+        #if number is 0, we dont need to divide, as it could give us NAN
+        if(validationSet[index2,i]!=0):
+            validationSet[index2,i]=abs(number2-meanVal)/standardDeviationVal
 
 def calculateCost(m, X, y):
     total = 0
-    for i in range(m):
+    for i in range(m-1):
         squared_error = (y[i] - X[i]) ** 2
         total += squared_error
     
@@ -45,52 +53,91 @@ def calculateCost(m, X, y):
 def sumDJ0(hTheta,y,m):
     sum=0
     for idx,yi in enumerate(y):
-        sum+=(hTheta-yi)
+        sum+=(hTheta[idx]-yi)
     return sum 
 
 
-def sumDJN(htheta,y,x,m):
+def sumDJN(hTheta,y,x,m):
     sum=0
     for idx,yi in enumerate(y):
-        sum+=(htheta-yi)*x[idx]
+        sum+=(hTheta[idx]-yi)*x[idx]
 
     return sum
 
 
-def calculateHtheta(allThetas,x):
+def calculateHtheta(allThetas,x,theta0):
     allThetas = allThetas.transpose()
-    newThetas = allThetas*x
+    #calculate htheta without theta0, so we can use the transpose and multiply method
+    newThetas =np.dot(allThetas,x)
+    #add theta0 back in
+    newThetas = newThetas+theta0
     return newThetas
 
-def calculateGradientDescent(x,y,theta0=0,theta1=0,alpha=0.01):
-    #define x as a numpy array
-    x= np.array(x)
-    m=len(y)
-    #calculating h theta by multiplying all x values by theta1,then adding theta0 to all x values
-    hTheta = calculateHtheta(theta0,theta1,x)
-    cost = calculateCost(m,hTheta,y)
+def getAllHthetas(thetas,inputs,numOfInputs,m):
+    hThetas = []
+    newInputs = []
+    thetasMinusTheta0=thetas[1:]
+    #calculate start value of htheta
+    #prevents us from going out of range when we calculate inputs[i][i+x]
+    for i in range(0,m):
+        #pull single row for h(theta)
+        for x in range(0,numOfInputs):
+            newInputs.append(inputs[x][i])
+        hThetas.append(calculateHtheta(thetasMinusTheta0,newInputs,thetas[0]))
+        #clear new inputs
+        newInputs=[]
+    return hThetas
+def calculateGradientDescent(inputs,output,thetas,numOfInputs,alpha=0.001,):
+    #using one array for input, array for thetas, loop to calculate new vals, loop to assign new vals
+    #define as a numpy array
+    m=len(inputs[0])
+    #get starting cost and hThetas
+    hThetas = getAllHthetas(thetas,inputs,numOfInputs,m)
+    cost = calculateCost(m,hThetas,output)
     while True:
+        sums =[]
+        #calculate new predicted values
+        DJ0=sumDJ0(hThetas,output,m)
+        sums.append(thetas[0] - (1/m) * alpha * DJ0)
+        #calculate theta 1 - theta n+1
+        for i in range(1,numOfInputs+1):
+            #inputs does not account for theta0, so we subtract one from its index
+            DJN = sumDJN(hThetas,inputs[i-1],output,m)
+            sums.append(thetas[i] - (1/m) * alpha * DJN)
         oldCost = cost
-        #calculate new thetas
-        sum1 = sumDJ0(hTheta,y,m)
-        sum2 = sumDJN(hTheta,y,x,m)
-        sum3 = sumDJN(hTheta,y,z,m)
-        temp0 = theta0 - (1/m) * alpha * sum1
-        temp1= theta1 - (1/m) * alpha * sum2
-        temp2 = theta2 - (1/m) * alpha * sum2
-        #recalculate h theta
-        #update together
-        theta0=temp0
-        theta1=temp1
-        theta2=temp2
-        hTheta = calculateHtheta(theta0,theta1,x)
-        cost = calculateCost(m,hTheta,y)
+        #assign values together 
+        sums = np.array(sums)
+        for i in range(0,numOfInputs+1):
+            temp = sums[i]
+            thetas[i] = temp
+        hThetas = getAllHthetas(thetas,inputs,numOfInputs,m)
+        cost = calculateCost(m,hThetas,output)
         #break if we've reached acceptable accuracy
         if(abs(cost-oldCost)<.01):
             break
-    return theta0,theta1
+    return thetas
 
+def predictValue1(newTheats,validationSet,numOfInputs):
+    sum=newTheats[0]+newTheats[1]*validationSet[6]+newTheats[2]*validationSet[9]
+    return sum
 
-
-
+#first calculation using two inputs
+inputs = np.array([trainingSet[:,6],trainingSet[:,9]])
+#add the two columns of inputs
+#output column
+output = np.array(trainingSet[:,13])
+thetas = np.array([0.0,0.0,0.0])
+newThetas = calculateGradientDescent(inputs,output,thetas,2)
+print(newThetas)
+# test = newThetas[0] + newThetas[1]*validationSet[0][0]+newTvalidationSet[0][1]
+# print("predicted values for validation set")
+# for i in range(0,49):
+#     print(f"predicted value for {i}: {predictValue1(newThetas,validationSet[i],2)}, actual value: {validationSet[i][13]}")
+# #calculation using all columns
+# inputs = np.array(trainingSet[:,:12])
+# output = np.array(trainingSet[:,13])
+# thetas = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+# newThetas = calculateGradientDescent(inputs,output,thetas,13)
+# for i in range(0,49):
+#     print(f"predicted value for {i}: {newThetas[i]}, actual value: {validationSet[i]}")
 
