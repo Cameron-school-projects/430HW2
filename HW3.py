88dcae6ca7611706724271803a907fd3fbb79ae4
Cameron Beck <cameron@pixelthin.com>
2024-03-02 21:02:06 -0600

    working on scalar value error

--- HW3.py
+++ HW3.py
@@ -1,15 +1,60 @@
 import numpy as np
-import pandas as pd
 from scipy.optimize import minimize
+class1Data = []
+class2And3Data = []
+#stating values for params/thetas
+params = np.hstack([0.0,0.0,0.0,0.0,0.0])
+#read in data
+with open('iris.data',encoding='utf-8') as f:
+    lineNum = 0
+    #skipping lines of header and non numerical data (first 22 rows)
+    for line in f:
+        #remove extra whitespace
+        line=line.strip()
+        lineData = line.split(",")
+        tempData = [float(lineData[0]),float(lineData[1]),float(lineData[2]),float(lineData[3])]
+        #first 50 entries are class 1, rest are class 2
+        if(lineNum<50):
+            tempData.append(1.0)
+            class1Data.append(tempData)
+        else:
+            tempData.append(0.0)
+            class2And3Data.append(tempData)
+        lineNum = lineNum+1
+#add in column of 1's for param 0
+class1Data = np.hstack((np.ones((len(class1Data), 1)), class1Data))
+class2And3Data= np.hstack((np.ones((len(class2And3Data), 1)), class2And3Data))
+#convert to numpy arrays
+class1Data = np.array(class1Data)
+class2And3Data = np.array(class2And3Data)
 
-#we need to read in the data and split it by class? get params for each set? 
-# or does Classifying 1 against 2 and 3 mean we classify as either class one, or of class 2/3? 
+#shuffle and get training and validation sets
+rng = np.random.default_rng()
+rng.shuffle(class1Data)
+rng.shuffle(class2And3Data)
+#20 percent of 50(for class 1) is 10, and 20 percent of 100 (class 2 ) is 20
+class1Validation = class1Data[(len(class1Data)-10):]
+class2And3Validation = class2And3Data[len(class2And3Data)-20:]
+class1Training = class1Data[:(len(class1Data)-10)]
+class2And3Training = class2And3Data[:len(class2And3Data)-20]
+#grab class attribute from training sets
+class1TrainingOutput = class1Training[:,5]
+class2And3TrainingOutput = class2And3Training[:,5]
+#remove class labels from training sets
+class1Training = np.delete(class1Training,5,1)
+class2And3Training = np.delete(class2And3Training,5,1)
+#grab class attribute for validation
+class1ValidationOutput = class1Validation[:,5]
+class2And3ValidationOutput = class2And3Validation[:,5]
+#remove class labels from validation sets
+class1Validation = np.delete(class1Validation,5,1)
+class2And3Validation = np.delete(class2And3Validation,5,1)
 
 def sigmoid(x, w, threshold=0.5):
-    p = 1 / (1 + np.exp(-x * w.transpose()))
+    p = 1 / (1 + np.exp(-np.matmul(x,w.transpose())))
     return np.where(p > threshold, 1, 0)
 
-#TP,FP,TN,FN should be the number of each kind of error occuring 
+#TP,FP,TN,FN should be the number of each kind of error occurring 
 def accuracy(TP, FP,TN,FN):
     return ((TP+TN)/(TP+TN+FP+FN))
 
@@ -17,12 +62,12 @@ def precision(TP,FP):
     return TP/(TP+FP)
 
 def logistic_loss(w, X, y):
-    sum = -(y * np.log(sigmoid(X, w)) + (1 - y) * np.log(1 - sigmoid(X, w)))
+    sum = -(y * np.log(sigmoid(X, w)) - (1 - y) * np.log(1 - sigmoid(X, w)))
+    print(sum)
     return sum/len(X)
 
 
 def logistic_loss_grad(w, X, y):
     return (X.transpose() * (sigmoid(X, w) - y)) / len(X)
-
 #this gets us a new set of W's which form the w array we pass into the sigmoid 
-# optimizedParams = minimize(logistic_loss, np.zeros(X.shape[1]), jac=logistic_loss_grad, args=(X, y), method="L-BFGS-B").x
+optimizedParams = minimize(logistic_loss, np.zeros(class1Training.shape[1]), jac=logistic_loss_grad, args=(class1Training, class1TrainingOutput), method="L-BFGS-B").x
