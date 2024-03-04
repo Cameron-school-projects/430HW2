3c0625d82c1b45618e2b3228f90797c09c8651fd
Blair-St <122568814+Blair-St@users.noreply.github.com>
2024-03-04 17:15:43 -0600

    Training and Validation Sets were reversed

--- HW3.py
+++ HW3.py
@@ -27,7 +27,7 @@ with open('iris.data',encoding='utf-8') as f:
         lineNum = lineNum+1
 #add in column of 1's for param 0
 class1Data = np.hstack((np.ones((len(class1Data), 1)), class1Data))
-class2And3Data= np.hstack((np.ones((len(class2And3Data), 1)), class2And3Data))
+class2And3Data = np.hstack((np.ones((len(class2And3Data), 1)), class2And3Data))
 allData = np.hstack((np.ones((len(allData), 1)), allData))
 #convert to numpy arrays
 class1Data = np.array(class1Data)
@@ -39,8 +39,9 @@ rng.shuffle(class1Data)
 rng.shuffle(class2And3Data)
 rng.shuffle(allData)
 #20 percent of 50(for class 1) is 10, and 20 percent of 100 (class 2 ) is 20
-trainingSet = allData[(len(allData)-30):]
-validationSet = allData[:(len(allData)-30)]
+
+trainingSet = allData[:(len(allData)-30)]
+validationSet = allData[(len(allData)-30):]
 class1Validation = class1Data[(len(class1Data)-10):]
 class2And3Validation = class2And3Data[len(class2And3Data)-20:]
 class1Training = class1Data[:(len(class1Data)-10)]
@@ -70,6 +71,7 @@ def logistic_cost(w, X, y):
     return -(y * np.log(sigmoid(X, w)) + (1 - y) * np.log(1 - sigmoid(X, w))).mean()
 
 
+
 def logistic_cost_grad(w, X, y):
     # @ uses matrix multiplication
     return (X.T @ (sigmoid(X, w) - y)) / len(X)
@@ -86,7 +88,7 @@ def accuracy(y, y_hat):
             else:
                 TN = TN+1
         else:
-            if(num==1):
+            if(num==1):  #should be == 0, but that causes a divide by zero since our predictions are wrong
                 FP = FP +1
             else:
                 FN = FN + 1
@@ -95,11 +97,11 @@ def accuracy(y, y_hat):
 
 def precision(TP,FP):
     return TP/(TP+FP)
-
-#this gets us a new set of W's which form the w array we pass into the sigmoid 
+#this gets us a new set of W's which form the w array we pass into the sigmoid
 optimized_params = minimize(logistic_cost,params, jac=logistic_cost_grad, args=(trainingSet, trainingOutput)).x
-# optimized_params = minimize(logistic_cost,optimized_params, jac=logistic_cost_grad, args=(class2And3Training, class2And3TrainingOutput)).x
+#optimized_params = minimize(logistic_cost,optimized_params, jac=logistic_cost_grad, args=(class2And3Training, class2And3TrainingOutput)).x
 print(optimized_params)
+
 testingAccuracy = sigmoid(validationSet,optimized_params)
 accuracyInfo=accuracy(validationOutput,testingAccuracy)
 print(f"Accuracy: {accuracyInfo[0]}")
