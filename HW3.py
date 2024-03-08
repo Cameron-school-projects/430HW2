79e9649cf4c56b296dced51b57c5473ea46568b8
Cameron Beck <cameron@pixelthin.com>
2024-03-08 15:35:43 -0600

    removed extra code and added space into iris.data

--- HW3.py
+++ HW3.py
@@ -1,10 +1,6 @@
 import numpy as np
 from scipy.optimize import minimize
-
-
-class1Data = []
 allData = []
-class2And3Data = []
 #stating values for params/thetas
 params = np.hstack([0.0,0.0,0.0,0.0,0.0])
 #read in data
@@ -19,50 +15,30 @@ with open('iris.data',encoding='utf-8') as f:
         #first 50 entries are class 1, rest are class 2
         if(lineNum<50):
             tempData.append(1.0)
-            class1Data.append(tempData)
             allData.append(tempData)
         else:
             tempData.append(0.0)
-            class2And3Data.append(tempData)
             allData.append(tempData)
             
         lineNum = lineNum+1
 #add in column of 1's for param 0
-class1Data = np.hstack((np.ones((len(class1Data), 1)), class1Data))
-class2And3Data = np.hstack((np.ones((len(class2And3Data), 1)), class2And3Data))
 allData = np.hstack((np.ones((len(allData), 1)), allData))
 #convert to numpy arrays
-class1Data = np.array(class1Data)
-class2And3Data = np.array(class2And3Data)
 allData = np.array(allData)
 #shuffle and get training and validation sets
 rng = np.random.default_rng()
-rng.shuffle(class1Data)
-rng.shuffle(class2And3Data)
 rng.shuffle(allData)
 #20 percent of 50(for class 1) is 10, and 20 percent of 100 (class 2 ) is 20
 
 trainingSet = allData[:(len(allData)-30)]
 validationSet = allData[(len(allData)-30):]
-class1Validation = class1Data[(len(class1Data)-10):]
-class2And3Validation = class2And3Data[len(class2And3Data)-20:]
-class1Training = class1Data[:(len(class1Data)-10)]
-class2And3Training = class2And3Data[:len(class2And3Data)-20]
 #grab class attribute from training sets
-class1TrainingOutput = class1Training[:,5]
-class2And3TrainingOutput = class2And3Training[:,5]
 trainingOutput = trainingSet[:,5]
 #remove class labels from training sets
-class1Training = np.delete(class1Training,5,1)
-class2And3Training = np.delete(class2And3Training,5,1)
 trainingSet = np.delete(trainingSet,5,1)
 #grab class attribute for validation
-class1ValidationOutput = class1Validation[:,5]
-class2And3ValidationOutput = class2And3Validation[:,5]
 validationOutput = validationSet[:,5]
 #remove class labels from validation sets
-class1Validation = np.delete(class1Validation,5,1)
-class2And3Validation = np.delete(class2And3Validation,5,1)
 validationSet = np.delete(validationSet,5,1)
 def sigmoid(x, w, threshold=0.5):
     # @ uses matrix multiplication
@@ -84,8 +60,6 @@ def logistic_cost_grad(w, X, y):
     return (X.T @ (sigmoid2(X, w) - y)) / len(X)
 
 def accuracy(y, y_hat):
-    print(y)
-    print(y_hat)
     TP = 0
     FP = 0
     FN = 0
@@ -107,11 +81,9 @@ def accuracy(y, y_hat):
 def precision(TP,FP):
     return TP/(TP+FP)
 #this gets us a new set of W's which form the w array we pass into the sigmoid
-print(trainingOutput)
-
 optimized_params = minimize(logistic_cost,params, jac=logistic_cost_grad, args=(trainingSet, trainingOutput)).x
 #optimized_params = minimize(logistic_cost,optimized_params, jac=logistic_cost_grad, args=(class2And3Training, class2And3TrainingOutput)).x
-print(optimized_params)
+print(f"The optimized logistic params are: {optimized_params}")
 
 testingAccuracy = sigmoid(validationSet,optimized_params)
 accuracyInfo=accuracy(validationOutput,testingAccuracy)
